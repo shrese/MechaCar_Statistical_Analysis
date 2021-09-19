32406dda5430e73a3823e9809242664ba5d9d723
shrese rudd <srudd1@hotmail.com>
2021-09-19 12:39:08 -0600

    adding deliverable 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,3 +1,5 @@
+###  Deliverable 1 ###
+
 ### 1. Download the MechaCar_mpg.csv file, and place it in the active directory for your R session.
 ### 2. Create a new RScript in your R source pane, name it MechaCarChallenge.RScript, and save it to your active directory.
 
@@ -17,3 +19,25 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mechacar))
 
 ### 7. Save your MechaCarChallenge.RScript file to your GitHub repository.
+
+###  Deliverable 2 ###
+
+### 1. Download the Suspension_Coil.csv file, and place it in the active directory for your R session.
+### 2. In your MechaCarChallenge.RScript, import and read in the Suspension_Coil.csv file as a table.
+coil <- read.csv('Suspension_Coil.csv', check.names = F, stringsAsFactors = F)
+head(coil)
+
+### 3. Write an RScript that creates a total_summary dataframe using the summarize() function to get the 
+###    mean, median, variance, and standard deviation of the suspension coil's PSI column.
+total_summary <- coil %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), Var_PSI=var(PSI), 
+  Std_Dev_PSI=sd(PSI), Num_Coil=n(), .groups = 'keep') 
+print(total_summary)
+
+### 4. Write an RScript that creates a lot_summary dataframe using the group_by() and the summarize() functions to 
+###    group each manufacturing lot by the mean, median, variance, and standard deviation of the suspension coil's 
+###    PSI column.
+lot_summary <- coil  %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI),
+  Var_PSI=var(PSI), Std_Dev_PSI=sd(PSI), Num_Coil=n(), .groups = 'keep') 
+print(lot_summary)
+
+### 5. Save your MechaCarChallenge.RScript file to your GitHub repository.
